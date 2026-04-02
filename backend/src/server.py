7aa764050081db8b0debecf3e96c4b49fcfd53a6
Samuel Velasquez <sjvisd@rit.edu>
2026-04-02 15:59:48 -0400

    added simple api skeleton

---
+++ backend/src/server.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_restful import Resource, Api
+from api.hello_world import HelloWorld
+from api.management import *
+
+app = Flask(__name__)
+api = Api(app)
+
+api.add_resource(Init, '/manage/init') #Management API for initializing the DB
+api.add_resource(Version, '/manage/version') #Management API for checking DB version
+api.add_resource(HelloWorld, '/') 
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
