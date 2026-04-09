b941ee5a61569194e9b2f812e3ff071e76bbb641
Samuel Velasquez <sjvisd@rit.edu>
2026-04-09 02:09:55 -0400

    changed dockerfile and db_utils to add and reference schema directory

--- backend/src/api/management.py
+++ backend/src/api/management.py
@@ -1,11 +1,11 @@
 from flask_restful import Resource, reqparse, request  #NOTE: Import from flask_restful, not python
 from db.db_utils import *
 
-from db.example import rebuild_tables
+from db.example import *
 
 class Init(Resource):
-    def post(self):
-        rebuild_tables()
+    def get(self):
+        list_examples()
 
 class Version(Resource):
     def get(self):
--- backend/src/server.py
+++ backend/src/server.py
@@ -2,7 +2,6 @@ from flask import Flask
 from flask_restful import Resource, Api
 from api.hello_world import HelloWorld
 from api.management import *
-
 app = Flask(__name__)
 api = Api(app)
 
@@ -12,4 +11,5 @@ api.add_resource(HelloWorld, '/')
 
 
 if __name__ == '__main__':
+    exec_sql_file('schema/schema.sql')
     app.run(host="0.0.0.0", port=5000)
