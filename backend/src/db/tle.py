dbd4c2f0311e6dd9b3bbe9eb20389f07033c6623
Samuel Velasquez <sjvisd@rit.edu>
2026-04-24 14:15:23 -0400

    propagation complete

--- backend/src/db/tle.py
+++ backend/src/db/tle.py
@@ -22,20 +22,3 @@ def listTLES():
     result_list.append(tle)
   
   return result_list
-
-def insertTLES(sat_id: int, sat_name: str, line_1: str, line_2: str):
-  """
-  Propagates TLE to Postgres database
-  sat_id: catalog number associated with satellite
-  sat_name: official satellite name
-  line_1: first line in TLE object
-  line_2: second line in TLE object
-  """
-
-  insert_sql = """
-    INSERT INTO tles(id, satellite_id, satellite_name, line1, line2)
-    VALUES (1, 1, 'TEST', 'LINE 1', 'LINE 2', NOW())
-    ON CONFLICT(id)
-    DO UPDATE SET 
-        inserted_at = NOW();
-  """
--- backend/src/server.py
+++ backend/src/server.py
@@ -4,8 +4,10 @@ from api.hello_world import HelloWorld
 from api.management import *
 from db.pull_tle import propagate_tles
 from db.db_utils import exec_sql_file
+from flask_apscheduler import APScheduler
 
 app = Flask(__name__)
+scheduler = APScheduler()
 api = Api(app)
 
 api.add_resource(Init, '/manage/init') #Management API for initializing the DB
@@ -16,4 +18,7 @@ api.add_resource(HelloWorld, '/')
 if __name__ == '__main__':
     exec_sql_file('schema/schema.sql')
     propagate_tles()
+    scheduler.init_app(app)
+    scheduler.add_job(id='daily_tle_propogation', func=propagate_tles, trigger='interval', hours=24)
+    scheduler.start()
     app.run(host="0.0.0.0", port=5000)
