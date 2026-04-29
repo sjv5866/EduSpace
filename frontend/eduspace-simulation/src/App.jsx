ea161554714bb14d75bf982a00fe6fc66a7d1d96
Samuel Velasquez <sjvisd@rit.edu>
2026-04-29 09:09:13 -0400

    realistic earth rotation, title, and static tle display

--- frontend/eduspace-simulation/src/App.jsx
+++ frontend/eduspace-simulation/src/App.jsx
@@ -1,22 +1,27 @@
 import React, { useRef } from "react";
 import { Canvas } from "@react-three/fiber";
 import Earth from './Earth';
-import { Stats, OrbitControls } from '@react-three/drei';
+import Title from './Title';
+import TLEGrid from './TLEGrid';
+import { OrbitControls } from '@react-three/drei';
 
 const App = () => {
   return (
-    <Canvas camera={{ position: [0, 0, 8.5], fov: 40 }} style={{
-      width: "100%",
-      height: "100vh",
-      objectFit: "cover",
-      backgroundImage: "url('./2k_stars.webp')",
-      backgroundSize: "cover",
-    }}>
-      <ambientLight intensity={0.4} color="#fceaff" />
-      <Earth position={[0, -0.1, 0]} />
-      <OrbitControls enablePan={false} enableDamping={false}/>
-      <Stats />
-    </Canvas>
+    <>
+      <Canvas camera={{ position: [0, 0, 8.5], fov: 40 }} style={{
+        width: "100%",
+        height: "100vh",
+        objectFit: "cover",
+        backgroundImage: "url('./2k_stars.webp')",
+        backgroundSize: "cover",
+      }}>
+        <ambientLight intensity={0.4} color="#fceaff" />
+        <Earth position={[0, -0.1, 0]} />
+        <OrbitControls enablePan={false} enableDamping={false}/>
+      </Canvas>
+      <Title />
+      <TLEGrid />
+    </>
   );
 };
 
