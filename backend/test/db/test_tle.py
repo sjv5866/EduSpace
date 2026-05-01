60f214a11f7008cc3bcd1de355e4dc7330ba46d3
Samuel Velasquez <sjvisd@rit.edu>
2026-05-01 01:38:48 -0400

    added minor test to unit test file

--- backend/test/db/test_tle.py
+++ backend/test/db/test_tle.py
@@ -8,3 +8,11 @@ class TestTLE(unittest.TestCase):
         """Build the tables"""
         result = exec_get_all('SELECT * FROM tles')
         self.assertEqual(28, len(result), "no rows in tles")
+
+    def test_get_tle_by_name(self):
+        "grab specific TLE from database"
+        result = get_tle_by_name("TEST")
+        self.assertEqual('TEST', result.satellite_name, 'cannot pull satellite name')
+        self.assertEqual('LINE 1', result.line1, 'cannot pull line1 from TLE')
+        self.assertEqual('LINE 2', result.line2, 'cannot pull line2 from TLE')
+        self.assertEqual('TEST', result.source, 'cannot pull source')
